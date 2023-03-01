1d9a247e08646cad5745272faa0de54cd1532c1c
josephchapman <9332991+josephchapman@users.noreply.github.com>
2023-03-01 20:40:23 +0000

    add vault, modify packer

--- src/vault/install.sh
+++ src/vault/install.sh
@@ -6,7 +6,9 @@ apt install -y \
     bash-completion \
     curl \
     gpg \
-    lsb-release
+    lsb-release \
+    libcap2-bin  # for `setcap`, otherwise vault installation shows:
+                 # /var/lib/dpkg/info/vault.postinst: line 36: setcap: command not found
 
 echo "Downloading Hashicorp GPG key ..."
 curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/hashicorp.gpg > /dev/null
@@ -17,20 +19,20 @@ tee /etc/apt/sources.list.d/hashicorp.list > /dev/null \
 deb [arch=amd64,signed-by=/etc/apt/trusted.gpg.d/hashicorp.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main
 EOF
 
-echo "Installing Packer ..."
+echo "Installing Vault CLI ..."
 apt-get update
 apt -y install \
-    packer
+    vault
 
 echo "Cleaning up ..."
 apt clean
 apt -y autoremove
 rm -rf /var/lib/apt/lists/*
 
-echo "Configuring Bash completion (packer) ..."
-# 'packer -autocomplete-install' just adds the 'complete' line to '.bashrc'
+echo "Configuring Bash completion (vault) ..."
+# 'vault -autocomplete-install' just adds the 'complete' line to '.bashrc'
 # The following line does it manually
-echo "complete -C /usr/bin/packer packer" | tee -a /etc/bash.bashrc > /dev/null
+echo "complete -C /usr/bin/vault vault" | tee -a /etc/bash.bashrc > /dev/null
 # non-login shell for all users
 
-echo "Packer installed"
+echo "Vault installed"
