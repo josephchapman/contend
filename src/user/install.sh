86fb6d36ef892dec74676b8594ab58ebabf713a9
josephchapman <9332991+josephchapman@users.noreply.github.com>
2023-03-01 20:38:39 +0000

    user fixes

--- src/user/install.sh
+++ src/user/install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Installing requirements ..."
+apt update
+apt install -y \
+    sudo
+
 echo "Configuring Login defs ..."
 # -E Extended Regex is required for '?'
 sed -i -E -e 's/^\s*(#\s*)?UMASK\s*\S*\s*$/UMASK		077/' /etc/login.defs
@@ -9,15 +14,25 @@ echo "Creating user ..."
 useradd -u ${UID} -U -G root -m -s /bin/bash ${USERNAME}
 sudo -u ${USERNAME} -g ${USERNAME} mkdir /home/${USERNAME}/.config/
 sudo -u ${USERNAME} -g ${USERNAME} touch /home/${USERNAME}/.bash_aliases
-
 sudo -u ${USERNAME} -g ${USERNAME} mkdir /home/${USERNAME}/bin/
-cat <<EOF | tee -a /home/${USERNAME}/.bash_aliases > /dev/null
+tee -a /home/${USERNAME}/.bash_aliases > /dev/null \
+<<EOF
 if [[ ":\${PATH}:" != *":/home/${USERNAME}/bin:"* ]]; then
   export PATH=\${PATH}:/home/${USERNAME}/bin
 fi
 EOF
 
-echo "PS1='[\[\033[1;32m\]\u\[\033[m\]@\[\033[1;35m\]\h\[\033[m\] \[\033[1;34m\]\W\[\033[m\]]\$ '" | tee -a /home/${USERNAME}/.bash_aliases > /dev/null
+tee -a /home/${USERNAME}/.bash_aliases > /dev/null \
+<<EOF
+PS1='[\[\033[1;32m\]\u\[\033[m\]@\[\033[1;35m\]\h\[\033[m\] \[\033[1;34m\]\W\[\033[m\]]\$ '
+EOF
+
+echo "Assigning groups ..."
+if [ -f /root/required_groups ]; then
+  while IFS="" read -r GROUP || [ -n "$GROUP" ]
+  do
+    usermod -aG ${GROUP} ${USERNAME}
+  done < /root/required_groups
+fi
 
 echo "User created"
-
